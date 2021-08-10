0f70a207239a9567cf050c732085a04f0f8aa6c9
Henrique Dezani <dezani@gmail.com>
2021-08-10 19:53:39 -0300

    Cria um adapter (console) do serviço

---
+++ Empresas.App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Empresas.Domain.Entities;
+
+namespace Empresas.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // criando um objeto da classe Empresa (instância da classe).
+            Empresa objeto = new Empresa();
+            objeto.SetNome(args[0]);            
+            string nome = objeto.GetNome();
+
+            Console.WriteLine(nome);
+        }
+    }
+}
