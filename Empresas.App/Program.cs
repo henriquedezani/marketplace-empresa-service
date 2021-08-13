53293aea539bc8d327aa96603be2ee8404f43f28
Henrique Dezani <dezani@gmail.com>
2021-08-13 18:19:38 -0300

    Refatora para usar propriedades

--- Empresas.App/Program.cs
+++ Empresas.App/Program.cs
@@ -10,15 +10,11 @@ namespace Empresas.App
             // criando um objeto da classe Empresa (instância da classe).
             //new Empresa() = // método para instanciar a classe = construtor.
 
-            Empresa empresa1 = new Empresa("Empresa 1"); 
-            Console.WriteLine(empresa1.GetNome());
-            Console.WriteLine(empresa1.GetSlug() ?? "NULO");
+            Empresa empresa1 = new Empresa("Empresa 1");
 
-            Empresa empresa2 = new Empresa("Empresa 2", "empresa_2");  // sobrecarga do construtor.
-            Console.WriteLine(empresa2.GetNome());
-            Console.WriteLine(empresa2.GetNome(true));
-            Console.WriteLine(empresa2.GetNome(false));
-            Console.WriteLine(empresa2.GetSlug() ?? "NULO");
+            empresa1.Nome = "Minha empresa"; // set
+
+            Console.WriteLine(empresa1.Nome); // get
         }
     }
 }
