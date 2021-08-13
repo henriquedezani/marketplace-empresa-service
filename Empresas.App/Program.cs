b5aac47301d961eb5244afb5d60286af5df35fcb
Henrique Dezani <dezani@gmail.com>
2021-08-13 18:35:04 -0300

    Uso do atalho 'prop' para criar propriedades

--- Empresas.App/Program.cs
+++ Empresas.App/Program.cs
@@ -10,11 +10,20 @@ namespace Empresas.App
             // criando um objeto da classe Empresa (instância da classe).
             //new Empresa() = // método para instanciar a classe = construtor.
 
-            Empresa empresa1 = new Empresa("Empresa 1");
+            Empresa empresa1 = new Empresa() {
+                Nome = "Empresa 1",
+                Endereco = "Rua ABC",
+                Telefone = "(17) 123123123"
+            };
 
-            empresa1.Nome = "Minha empresa"; // set
+            // empresa1.Nome = "Minha empresa"; // set
+            // empresa1.Endereco = "Minha empresa"; // set
+            // empresa1.Telefone = "Minha empresa"; // set
+
 
             Console.WriteLine(empresa1.Nome); // get
+            Console.WriteLine(empresa1.Endereco); // get
+            Console.WriteLine(empresa1.Telefone); // get
         }
     }
 }
--- Empresas.Domain/Entities/Empresa.cs
+++ Empresas.Domain/Entities/Empresa.cs
@@ -3,15 +3,19 @@ namespace Empresas.Domain.Entities
     public class Empresa 
     {
         // propridade automática (campo + propriedade)
-        // public string Nome { get; private set; }
-        public string Nome { get; }
-        private string slug;
+        // public string Nome { get; private set; } // read-only
+        // public string Nome { get; } // read-only
+        public string Nome { get; set; }
+        public int Id { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+
 
         // construtor
-        public Empresa(string nome)
-        {
-            this.Nome = nome;
-        }
+        // public Empresa(string nome)
+        // {
+        //     this.Nome = nome;
+        // }
 
         // propriedades:
         // é o que o compilador gerará usando public string Nome {get; set;}
