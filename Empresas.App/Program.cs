303381ff4a09a0bedb3c89ad48fb5b36ea080af8
Henrique Dezani <dezani@gmail.com>
2021-08-17 17:01:06 -0300

    Finaliza criação da entidade Empresa

--- Empresas.App/Program.cs
+++ Empresas.App/Program.cs
@@ -7,23 +7,20 @@ namespace Empresas.App
     {
         static void Main(string[] args)
         {
-            // criando um objeto da classe Empresa (instância da classe).
-            //new Empresa() = // método para instanciar a classe = construtor.
-
             Empresa empresa1 = new Empresa() {
                 Nome = "Empresa 1",
                 Endereco = "Rua ABC",
-                Telefone = "(17) 123123123"
+                Telefone = "(17) 123123123",
+                Id = 1,
+                Slug = "empresa1"
             };
 
-            // empresa1.Nome = "Minha empresa"; // set
-            // empresa1.Endereco = "Minha empresa"; // set
-            // empresa1.Telefone = "Minha empresa"; // set
 
-
+            Console.WriteLine(empresa1.Id); // get
             Console.WriteLine(empresa1.Nome); // get
             Console.WriteLine(empresa1.Endereco); // get
             Console.WriteLine(empresa1.Telefone); // get
+            Console.WriteLine(empresa1.Slug); // get
         }
     }
 }
--- Empresas.Domain/Entities/Empresa.cs
+++ Empresas.Domain/Entities/Empresa.cs
@@ -2,28 +2,10 @@ namespace Empresas.Domain.Entities
 {
     public class Empresa 
     {
-        // propridade automática (campo + propriedade)
-        // public string Nome { get; private set; } // read-only
-        // public string Nome { get; } // read-only
-        public string Nome { get; set; }
         public int Id { get; set; }
+        public string Nome { get; set; }
         public string Endereco { get; set; }
         public string Telefone { get; set; }
-
-
-        // construtor
-        // public Empresa(string nome)
-        // {
-        //     this.Nome = nome;
-        // }
-
-        // propriedades:
-        // é o que o compilador gerará usando public string Nome {get; set;}
-        // private string nome;
-        // public string Nome
-        // {
-        //     get { return this.nome; } 
-        //     set { this.nome = value; }
-        // }
+        public string Slug { get; set; }
     }
 }
