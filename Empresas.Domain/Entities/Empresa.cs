288291d123afcc40295c74247af21f8314f6cbaa
Henrique Dezani <dezani@gmail.com>
2021-08-20 16:55:08 -0300

    Adicionada classe Empresa

--- Empresas.Domain/Entities/Empresa.cs
+++ Empresas.Domain/Entities/Empresa.cs
@@ -1,29 +1,22 @@
+using System.Linq;
+using System.Collections.Generic;
+
 namespace Empresas.Domain.Entities
 {
     public class Empresa 
     {
-        // propridade automática (campo + propriedade)
-        // public string Nome { get; private set; } // read-only
-        // public string Nome { get; } // read-only
         public string Nome { get; set; }
         public int Id { get; set; }
         public string Endereco { get; set; }
         public string Telefone { get; set; }
+        public string Slug { get; set; }
+        public IEnumerable<Produto> Produtos { get; set; } = new List<Produto>();
 
-
-        // construtor
-        // public Empresa(string nome)
-        // {
-        //     this.Nome = nome;
-        // }
-
-        // propriedades:
-        // é o que o compilador gerará usando public string Nome {get; set;}
-        // private string nome;
-        // public string Nome
-        // {
-        //     get { return this.nome; } 
-        //     set { this.nome = value; }
-        // }
+        public void AddProduto(Produto produto)
+        {
+            var lista = Produtos.ToList();
+            lista.Add(produto);
+            this.Produtos = lista;
+        }
     }
 }
